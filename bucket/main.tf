e25f0191fcb4ebd2652be693373ceea769c1d2b0
camilomontoyau <camilo.montoya@techstars.com>
2023-07-23 17:36:21 -0500

    check

--- bucket/main.tf
+++ bucket/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_backend" {
+resource "aws_s3_bucket" "terraform_backend_files" {
   bucket = var.bucket_name
   acl    = var.bucket_acl
   tags   = var.bucket_tags
