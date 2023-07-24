58f3ae2c118e190bdeb2b84888ee187e8d8dce4d
camilomontoyau <camilo.montoya@techstars.com>
2023-07-24 00:05:00 -0500

    reactivate DB

--- myapp/src/app.module.ts
+++ myapp/src/app.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
-//import { TypeOrmModule } from '@nestjs/typeorm';
-//import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  // imports: [
-  //   TypeOrmModule.forRoot({
-  //     type: 'postgres',
-  //     host: 'my-postgres.default.svc.cluster.local',
-  //     // host: 'localhost',
-  //     port: 5432,
-  //     username: 'test',
-  //     password: 'test',
-  //     database: 'test',
-  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //     synchronize: true,
-  //     retryAttempts: 10,
-  //     retryDelay: 10000
-  //   }),
-  //   UsersModule,
-  // ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'my-postgres.default.svc.cluster.local',
+      // host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 10000
+    }),
+    UsersModule,
+  ],
   providers: [AppService],
   controllers: [AppController],
 })
