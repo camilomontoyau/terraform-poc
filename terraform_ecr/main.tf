a135f7d62649940ae79c3060af854270e9ebcf55
camilomontoyau <camilo.montoya@techstars.com>
2023-07-25 15:30:57 -0500

    terraform_ecr initial config

---
+++ terraform_ecr/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  for_each = toset(var.ecr_repo_names)
+  name = each.key
+  image_tag_mutability = var.image_tag_mutability
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+  encryption_configuration {
+    encryption_type = var.encryption_type
+  }
+  tags = var.tags
+}
---
+++ terraform_ecr/variables.tf
@@ -0,0 +1,30 @@
+variable "ecr_repo_names" {
+    description = "The name of the ECR registry"
+    type = any
+    default = null
+}
+
+variable "image_tag_mutability" {
+    description = "The tag mutability setting for the repository. Must be one of: MUTABLE or IMMUTABLE"
+    type = string
+    default = "MUTABLE"
+}
+
+variable "scan_on_push" {
+    description = "Indicates whether images are scanned after being pushed to the repository (true) or not scanned (false)"
+    type = bool
+    default = true
+}
+
+variable "encryption_type" {
+    description = "The encryption type to use for the repository. Must be one of: AES256 or KMS"
+    type = string
+    default = "KMS"
+}
+
+variable "tags" {
+    description = "A mapping of tags to assign to the resource"
+    type = map(string)
+    default = {}
+}
+  
